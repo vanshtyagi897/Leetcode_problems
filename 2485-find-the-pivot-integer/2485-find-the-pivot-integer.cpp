bff9523a87280c0a2d4c6885133e9ed3368a0871
Vansh Tyagi <144048348+vanshtyagi897@users.noreply.github.com>
2026-08-04 19:50:03 +0530

    Time: 0 ms (100%), Space: 8 MB (71.59%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int>arr;
+        int totalSum= n*(n+1)/2;
+        int lsum=0, rsum=0;
         for(int i=1;i<=n;i++){
-            arr.push_back(i);
-        }
-       
-        for(int i=0;i<n;i++){
-            int lsum=0;
-            for(int j=0;j<=i;j++){
-                lsum+=arr[j];
-            }
-            int rsum=0;
-            for(int k=i;k<n;k++){
-                rsum+=arr[k];
-            }
-            if(lsum==rsum) return arr[i];
+            lsum += i;
+            rsum = totalSum - lsum + i;
+            if(lsum == rsum) return i;
         }
         return -1;
 
