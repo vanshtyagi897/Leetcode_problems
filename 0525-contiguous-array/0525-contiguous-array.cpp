d5054aaae03617d5698e1100d474b4a684ccb033
Vansh Tyagi <144048348+vanshtyagi897@users.noreply.github.com>
2026-08-08 11:30:53 +0530

    Time: 29 ms (25.91%), Space: 88.1 MB (93.65%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         //replace all 0s with -1
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) nums[i]=-1;
-        }
         //now question becomes: find max len of subarray whose sum is 0
         unordered_map<int,int>mp;
         mp[0]=-1;
         int sum=0;
         int maxLen=0;
         for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+            if(nums[i]==0) sum+=-1;
+            else sum+=1;
+            
             if(mp.find(sum)!=mp.end()){
                 maxLen=max(maxLen,i-mp[sum]);
             }
